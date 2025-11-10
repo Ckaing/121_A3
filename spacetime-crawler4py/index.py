033f61c8c78c357fbdd41baa25aecc2e1ecf9eaf
wuuhbee <melynlim05@gmail.com>
2025-11-10 10:13:43 -0800

    added function to be able to read from file, add a posting using token, url and freq, and changed write to file format to correlate with reading from

--- spacetime-crawler4py/index.py
+++ spacetime-crawler4py/index.py
@@ -9,14 +9,37 @@ class Index:
         if (token not in self.index):
             self.index[token] = Posting()
 
+    def add_posting(self, token, urlid, freq):
+        if (token not in self.index):
+            print("Token not in index. Failed to add frequency.")
+            return
+        post = self.index[token]
+        post.add_entry(urlid, freq)
+
     def write_to_file(self, file):
         with open(file, "w") as outfile:
-            for url, id in enumerate(self.index):
-                print(f"{id} - {url}", file=outfile)
+            for i, (token, post) in enumerate(self.index.items()):
+                print(f"{token} -> {post}", file=outfile)
         self._reset()
 
     def _reset(self):
         self.index = {}
+
+    def fill_index(self, file):
+        if len(self.index) > 0:
+            print("Index already filled.")
+            return
+        with open(file, "r") as infile:
+            # each line is in the format [token] -> id-freq id-freq id-freq
+            for line in infile:
+                # split line to separate the token and postings
+                lines = line.split(" -> ")
+                self.add_entry(lines[0])
+                # split the postings to get the info
+                posts = lines[1].split()
+                for p in posts:
+                    entry = p.split('-')
+                    self.add_posting(lines[0], entry[0], entry[1])            
 
 
 class URLIndex(Index):
