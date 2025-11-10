b280a8452c0018a86177fa750e38b90dfb039819
wuuhbee <melynlim05@gmail.com>
2025-11-10 08:46:37 -0800

    added new class for indexing urls to assign ids and function in Index class to write to file and reset the index for next batch

--- spacetime-crawler4py/index.py
+++ spacetime-crawler4py/index.py
@@ -5,6 +5,29 @@ class Index:
     def __init__(self):
         self.index = {}
 
-    def add_token(self, token):
+    def add_entry(self, token):
         if (token not in self.index):
             self.index[token] = Posting()
+
+    def write_to_file(self, file):
+        with open(file,"w") as outfile:
+            for url, id in enumerate(self.index):
+                print(f"{id} - {url}", file=outfile)
+        self._reset()
+
+    def _reset(self):
+        self.index = {}
+
+
+class URLIndex(Index):
+    def __init__(self):
+        super().__init__()
+        self.count = 1
+
+    def add_entry(self, url):
+        if (url not in self.index.keys()):
+            self.index[url] = self.count
+            self.count += 1
+
+    def get_id(self, url):
+        return self.index[url]
