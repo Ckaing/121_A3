c49e15e2314a158dfe5bbb21fecc490e3fae8adc
wuuhbee <melynlim05@gmail.com>
2025-11-10 08:54:45 -0800

    removed fields paramter in add_entry

--- spacetime-crawler4py/postings.py
+++ spacetime-crawler4py/postings.py
@@ -2,5 +2,5 @@ class Posting:
     def __init__(self):
         self.posting = {}
 
-    def add_entry(self, docID, tfidf, fields):
+    def add_entry(self, docID, tfidf):
         self.posting[docID] = tfidf
