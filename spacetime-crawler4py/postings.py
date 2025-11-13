2f4ce6899a8ab94756a468b8f6bda5ebf0adb6eb
wuuhbee <melynlim05@gmail.com>
2025-11-13 08:25:27 -0800

    added information to the postings that we need to keep track of: fields and positions

--- spacetime-crawler4py/postings.py
+++ spacetime-crawler4py/postings.py
@@ -1,9 +1,13 @@
 class Posting:
     def __init__(self):
         self.posting = {}
+        self.fields = {}
+        self.position = {}
 
-    def add_entry(self, docID, tfidf):
-        self.posting[docID] = tfidf
+    def add_entry(self, docID, freq, fields : str[], positions : int[]):
+        self.posting[docID] = freq
+        self.fields[docID] = fields
+        self.position[docID] = positions
 
     def __str__(self) -> str:
         post = []
