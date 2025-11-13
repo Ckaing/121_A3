e0895b9778333028205d585b348864a8a5d58bdb
Ckaing <kaingc@uci.edu>
2025-11-13 11:28:34 -0800

    added custom encoder for jsons

--- spacetime-crawler4py/index.py
+++ spacetime-crawler4py/index.py
@@ -1,27 +1,48 @@
 from postings import Posting
+import json
 
+def custom_encoder(obj):
+    if isinstance(obj, Posting):
+        return {
+            'freq': obj.freq,
+            'fields': obj.fields,
+            'position': obj.position
+        }
+    raise TypeError("Object of type %s is not JSON serializable" % type(obj).__name__)
+
+def custom_decoder(dct):
+    if 'freq' in dct and 'fields' in dct and 'position' in dct:
+        post = Posting()
+        post.freq = dct['freq']
+        post.fields = dct['fields']
+        post.position = dct['position']
+        return post
+    return dct
 
 class Index:
     def __init__(self):
         self.index = {}
 
-    def add_token(self, token):
+    def add_entry(self, token):
         """Initializes token into self.index with a Posting object
         if not already initialized."""
         if (token not in self.index):
-            self.index[token] = {}
+            self.index[token] = Posting()
 
-    def add_posting(self, token, docid):
-        """Adds Posting obj to specified token with associated docID."""
+    def add_posting(self, token, urlid, freq):
+        """Adds entry into the Posting object associated with token, using
+        the docID and token frequency in that docID."""
         if (token not in self.index):
-            self.index[token] = {}
-        if (docid not in self.index[token].keys()):
-            self.index[token][docid] = Posting()
+            print("Token not in index. Failed to add frequency.")
+            return
+        post = self.index[token]
+        post.add_entry(urlid, freq)
 
     def write_to_file(self, file):
-        """Writes the information in self.index into a file. Clears index
+        """Writes the information in self.index into a json file. Clears index
         for next batch to be read in."""
-        #write here and reset after
+        with open(file, "w") as outfile:
+            outfile.write(json.dumps(self.index, default=custom_encoder))
         self._reset()
 
     def _reset(self):
@@ -34,20 +55,32 @@ class Index:
         index before reading if index is already filled."""
         if len(self.index) > 0:
             self._reset()
-        # read back into the object here
+        with open(file, "r") as infile:
+            # each line is in the format [token] -> id-freq id-freq id-freq
+            for line in infile:
+                # split line to separate the token and postings
+                lines = line.split(" -> ")
+                self.add_entry(lines[0])
+                # split the postings to get the info
+                posts = lines[1].split()
+                for p in posts:
+                    entry = p.split('-')
+                    self.add_posting(lines[0], entry[0], entry[1])
     
     # TODO: write this function similar to fill_index but with additional
     # checks for whether it is already in the index. Idk if we want to write
     # it back to file at the end but I was thinking that we merge after every
     # batch of urls we parse.
     def merge(self, file):
-        """Read from file and merges the tokens we read with the tokes in memory"""
+        """Read from file. If the token is in the current index, add the
+        postings from the token to current index. If token not in current
+        index, add token."""
 
 
-class URLIndex:
+class URLIndex(Index):
     def __init__(self):
-        self.index = {}
-        self.id = 0
+        super().__init__()
+        self.id = 1
 
     def add_entry(self, url):
         """Adds a url to the index if not already in the list.
