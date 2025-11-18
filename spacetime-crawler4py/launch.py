9b57d18040c898878c28f5686a25e1acfbcde474
wuuhbee <melynlim05@gmail.com>
2025-11-18 14:23:01 -0800

    saves the remainder of the batch to disk and then merge all buckets together before printing report to file

--- spacetime-crawler4py/launch.py
+++ spacetime-crawler4py/launch.py
@@ -9,7 +9,7 @@ from argparse import ArgumentParser
 from utils.config import Config
 from crawler import Crawler
 from index_vars import json_index, URL_id_index
-from analyze import write_analysis_to_file
+from analyze import write_analysis_to_file, indexer, batch_cnt, BATCH_SIZE
 from query import query, user_input, extract_terms
 
 
@@ -31,8 +31,19 @@ def main(config_file, restart, json_dir=None):
 
     crawler = Crawler(config, restart)
     crawler.start()
+    
+    # makes sure to save the rest of the documents
+    if (batch_cnt % BATCH_SIZE != 0):
+        indexer.save_batch_to_disk()
+    # finally merge all the buckets into one index
+    indexer.merge_all_buckets(cleanup_temp=True)
 
     # write our analysis when our crawler ends
+    print("\n--- Final Statistics ---", file="report.txt")
+    stats = indexer.get_final_stats()
+    for bucket, info in sorted(stats.items()):
+        print(f"Bucket '{bucket}': {info['terms']} terms, {info['file_size_mb']:.2f} MB", file="report.txt")
+
     json_index.write_to_file(file="inverted_index.json")
     URL_id_index.write_to_file(file="url_id_index.json")
     write_analysis_to_file()
