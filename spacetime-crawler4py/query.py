bf86b3f311c2f73409b63029f83389f05bf5f49c
wuuhbee <melynlim05@gmail.com>
2025-11-18 10:40:14 -0800

    added more notes to query() and imported stemmer to normalize the query

--- spacetime-crawler4py/query.py
+++ spacetime-crawler4py/query.py
@@ -1,3 +1,5 @@
+from tokenizer import tokenize, stemmer
+
 def token_file(token):
     """Returns the file name that the token might be found in."""
     return f"index/{token[0]}"
@@ -18,4 +20,21 @@ def query(q):
     """Given a list of terms, check respective files for the token and
     then return the postings for each token and check intersections of
     the postings. Returns the top 5 urls."""
+    len_q = len(q)
+    # replace the words in q with its stem
+    for i in range(len_q):
+        q[i] = stemmer.stem(q[i])
+
+    # each element is a list of docids for each term
+    postings = []
+    for word in q:
+        # get the file name of where the token would be
+        file = token_file(word)
+        # open the file, retrieve index, find token and insert the keys
+        # (docid) into postings
     
+    # save the top urls
+    urls = []
+    # search the postings for matching documents
+    
+    return urls
